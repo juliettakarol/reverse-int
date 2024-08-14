c17458d135e213796247be1cc4eab77b60449a9e
Julyetta Karol <julietta_karol@mail.ru>
2024-08-14 15:33:44 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    function isNagativeNumber(number) {
+        if (number < 0) {
+            return number * (-1)
+        } else {
+            return number
+        }
+    }
+
+    function isFirstNull(number0) {
+        if (number0[0] === '0') {
+            return number0.slice(1)
+        } else {
+            return number0
+        }
+    }
+
+    function reverseNumber(name) {
+        const nqq = String(isNagativeNumber(name)).split('').reverse().join('')
+        return Number(isFirstNull(nqq))
+    }
+
+    return reverseNumber(n)
 }
+
+
+
+
